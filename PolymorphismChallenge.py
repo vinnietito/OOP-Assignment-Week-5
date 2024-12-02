1a35118e561331c10df6bba7e8b77f14935d02bb
vinnietito <vincentnjuguna69@gmail.com>
2024-12-02 18:05:06 +0300

    Commits

--- PolymorphismChallenge.py
+++ PolymorphismChallenge.py
@@ -2,3 +2,10 @@
 class Vehicle:
     def move(self):
         raise NotImplementedError("SUbclasses must implement this method!")
+    
+# Subclass for Car
+class Car(Vehicle):
+    def move(self):
+        return "Driving on the road"
+    
+    
